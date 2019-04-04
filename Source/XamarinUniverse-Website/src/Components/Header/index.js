78f71bb19b295c6363e9dbcfa8ca561abacf7b5a
Adonis Mendoza <adonis.mendoza@outlook.com>
2019-04-04 08:50:55 -0400

    Create Template in React #1

---
+++ Source/XamarinUniverse-Website/src/Components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Welcome to Xamarin Universe Website.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
---
+++ Source/XamarinUniverse-Website/src/Pages/Main/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from '../../Components/Header';
+import './index.css';
+
+export default class Main extends Component {
+  render() {
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    );
+  }
+}
